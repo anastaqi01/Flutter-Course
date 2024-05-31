1cb75117a488b17411868a46cf76b11d25c1fa12
anastaqi01 <annustaqi11@gmail.com>
2024-05-31 20:07:26 +0500

    Create list-practice.dart

---
+++ list-practice.dart
@@ -0,0 +1,11 @@
+void main() {
+  var studentNames = ["anas", "wali", "faizan", 23, true];
+  List<String> list = ["23", "12", "1231"];
+  List<int> integerList = [23, 12, 1231];
+  List<bool> boolList = [true, false, false, true];
+  print(studentNames[4]);
+  print(studentNames.length); // length function of List
+  print(list[2]);
+  print(integerList[2]);
+  print(boolList[2]);
+}
