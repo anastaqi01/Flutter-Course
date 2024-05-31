d45747918179e344d165c4a046e5b81370fb53dc
anastaqi01 <annustaqi11@gmail.com>
2024-05-31 18:54:58 +0500

    created data-types.dart

---
+++ data-types.dart
@@ -0,0 +1,19 @@
+void main() {
+  String name = "Anas";
+  print(name);
+  int a = 10;
+  print(a);
+  num b = 5;
+  print(b);
+  num c = 16;
+  print(c);
+  num result = b * c;
+  print(result);
+  num result1 = c ~/ a;
+  print(result1);
+  num result2 = b % c;
+  print(result2);
+  bool res = result1 > result2;
+  print(res);
+  print(a != b);
+}
