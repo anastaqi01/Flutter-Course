b08ba21638cc779bb7d91430cf481bd860c80acb
anastaqi01 <annustaqi11@gmail.com>
2024-06-06 19:17:18 +0500

    update practice 2 dart complete
    updated practice-questions.dart filewith dart practice 2 questions complete

--- practice-questions.dart
+++ practice-questions.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'dart:io';
 
 void main() {
@@ -100,10 +99,10 @@ void main() {
   }
 
 // Write a dart program to print your name 100 times.
-  // String myName = "Anas Taqi";
-  // for (var i = 0; i < 100; i++) {
-  //   print("My Name is ${myName}");
-  // }
+  String myName = "Anas Taqi";
+  for (var i = 0; i < 100; i++) {
+    print("My Name is ${myName}");
+  }
 
 // Write a dart program to calculate the sum of natural numbers.
   int naturalNumber = 1;
@@ -113,8 +112,45 @@ void main() {
     naturalNumber++;
   }
   print("the sum of first 10 natural number is: ${sum}");
+
 // Write a dart program to generate multiplication tables of 5.
+  int value = 5;
+  for (int i = 1; i < 11; i++) {
+    print("${value} * ${i} = ${(value * i)}");
+  }
+
 // Write a dart program to generate multiplication tables of 1-9.
+  for (int i = 1; i < 10; i++) {
+    // it will loop once while next loop will loop 10 times.
+    for (int j = 1; j < 11; j++) {
+      print("${i} * ${j} = ${(i * j)}");
+    }
+  }
+
 // Write a dart program to create a simple calculator that performs addition, subtraction, multiplication, and division.
+  print("enter 1st number:");
+  int? item1 = int.parse(stdin.readLineSync()!);
+  print("enter 2nd number:");
+  int? item2 = int.parse(stdin.readLineSync()!);
+  print("enter you operator: +, -, *, /");
+  String? operators = stdin.readLineSync()!;
+  if (operators.contains("+")) {
+    print("Sum of ${item1} and ${item2} = ${(item1 + item2)}");
+  } else if (operators.contains("-")) {
+    print("Subtraction of ${item1} and ${item2} = ${(item1 - item2)}");
+  } else if (operators.contains("*")) {
+    print("Multiplication of ${item1} and ${item2} = ${(item1 * item2)}");
+  } else if (operators.contains("/")) {
+    print("Division of ${item1} and ${item2} = ${(item1 / item2)}");
+  } else {
+    print("Wrong Operator Selected");
+  }
+
 // Write a dart program to print 1 to 100 but not 41.
+  for (int i = 1; i < 101; i++) {
+    if (i == 41) {
+      continue;
+    }
+    print(i);
+  }
 }
