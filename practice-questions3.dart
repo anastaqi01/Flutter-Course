5d49f8cc638901f701146e29b78109213c816f19
anastaqi01 <annustaqi11@gmail.com>
2024-06-10 14:40:23 +0500

    update practice-question 3 file

--- practice-questions3.dart
+++ practice-questions3.dart
@@ -64,9 +64,58 @@ void main() {
   print(ans);
 
 // Write a program in Dart to calculate power of a certain number. For e.g 5^3=125
+  void cube(int val, int power) {
+    num cubed = pow(val, power);
+    print(cubed);
+  }
+
+  cube(5, 3);
+
 // Write a function in Dart named add that takes two numbers as arguments and returns their sum.
+  void add(int a, int b) {
+    print("Sum of ${a} and ${b} is: ${(a + b)}");
+  }
+
+  add(13, 12);
+
 // Write a function in Dart called maxNumber that takes three numbers as arguments and returns the largest number.
+  void maxNumber(int a, int b, int c) {
+    List<int> max = [a, b, c];
+    int maximum = 0;
+    for (int i = 0; i < max.length - 1; i++) {
+      for (int j = 0; j < max.length - 1; j++) {
+        if (max[i] > max[j]) {
+          maximum = max[i];
+        }
+      }
+    }
+    print(maximum);
+  }
+
+  maxNumber(321, 123, -20);
 // Write a function in Dart called isEven that takes a number as an argument and returns True if the number is even, and False otherwise.
+  bool isEven(int val) {
+    if (val % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  print(isEven(32));
+
 // Write a function in Dart called createUser with parameters name, age, and isActive, where isActive has a default value of true.
+  void createUser(String name, int age, [bool isActive = true]) {
+    print("${name}, ${age},${isActive}");
+  }
+
+  createUser("anas", 18);
+
 // Write a function in Dart called calculateArea that calculates the area of a rectangle. It should take length and width as arguments, with a default value of 1 for both. Formula: length * width.
+  void calculateArea([double length = 1, width = 1]) {
+    print(length * width);
+  }
+
+  calculateArea(); // using no value to see function using default values.
+  calculateArea(20, 30); // default values overwritten by provied values.
 }
